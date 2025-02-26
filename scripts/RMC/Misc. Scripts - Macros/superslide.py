8bdbae6d5d438e8a5f564c88225d85dc5f6c4d8a
Epik95mkw <epik955@gmail.com>
2025-02-25 20:15:16 -0500

    Superslide (and neutral glide) macro

---
+++ scripts/RMC/Misc. Scripts - Macros/superslide.py
@@ -0,0 +1,19 @@
+from dolphin import controller, event # type: ignore
+import Modules.mkw_classes as mkw
+from Modules.macro_utils import MKWiiGCController
+
+
+@event.on_frameadvance
+def on_frame_advance():
+    if mkw.RaceManager().state() != mkw.RaceState.RACE:
+        return
+    if not mkw.KartSettings.is_bike():
+        return
+
+    ctrl = MKWiiGCController(controller)
+    kart_move = mkw.KartMove()
+
+    if abs(kart_move.lean_rot()) + kart_move.lean_rot_increase() > kart_move.lean_rot_cap():
+        ctrl.set_inputs({
+            "StickX": 0,
+        })
