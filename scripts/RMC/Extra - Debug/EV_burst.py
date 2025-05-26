2c4779f98c1bfbc3ce69020fe0e7d3d3525b8927
Blounard <Blounard@hotmail.fr>
2025-05-26 20:25:57 +0200

    debug script
    not available for all regions

---
+++ scripts/RMC/Extra - Debug/EV_burst.py
@@ -0,0 +1,34 @@
+from dolphin import event, savestate, utils
+import os
+from pathlib import Path
+import Modules.mkw_utils as mkw_utils
+import Modules.framesequence as framesequence
+import Modules.ttk_lib as ttk_lib
+from Modules.mkw_translations import vehicle_id
+import Modules.mkw_classes as mkw 
+
+import time
+
+def main():
+    #savestate.load_from_slot(2)
+
+    mkw_utils.add_angular_ev(0, 0, 10, 0)
+    global end
+    end = False
+
+
+
+
+@event.on_frameadvance
+def frameadvance():
+    global end
+    global g_save_file_name
+   
+    if (not end) and mkw_utils.extended_race_state() > 0:
+        utils.cancel_script(utils.get_script_name())
+        end = True
+
+
+
+if __name__ == '__main__':
+    main()
