35a80806b9996c37898f486ee1bc8b2d15dc4a5b
Blounard <Blounard@hotmail.fr>
2025-05-27 17:43:24 +0200

    attempt to fix linux import
    linux had broken import, this is an attempt at fixing it

---
+++ scripts/Modules/__init__.py
@@ -0,0 +1,18 @@
+import mkw_classes
+import marco_utils
+import settings_utils
+import input_display
+
+import mkw_translation
+import mkw_utils
+import framesequence
+
+import ttk_config
+import rkg_lib
+
+import ttk_lib
+
+import agc_lib
+import bruteforcer_lib
+import startslide_utils
+import infodisplay_utils
