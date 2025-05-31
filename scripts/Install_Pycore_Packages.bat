70cab37c108cc0690e080d9b106ec24411887ff7
Blounard <Blounard@hotmail.fr>
2025-05-31 15:16:09 +0200

    updater update
    made the updater better. A single use of "Update_package_+_Scripts" does everything

--- scripts/Install_Pycore_Packages.bat
+++ scripts/Install_Pycore_Packages.bat
@@ -3,7 +3,6 @@ pip install numpy
 pip install matplotlib
 pip install pillow
 pip install pygame
-pip install requests
 pip install moviepy
 python verify_package.py
 pause
---
+++ scripts/Update_Packages_+_Scripts.bat
@@ -0,0 +1,4 @@
+python -m pip install --upgrade pip
+pip install requests
+python update_scripts.py
+./Install_Pycore_Packages.bat
